4cc196e0ec440f95a727f32acc0954ba0fdc33f1
NabilEMAP <Nabil_EM@outlook.com>
2023-10-12 15:21:31 +0200

    Added Mapper.cs
    - Added Mapper.cs in the PlanningsTool.BLL
    - Combining PlanningsTool.DAL.Models entities with PlanningsTool.Common DTO's

---
+++ PlanningsTool/PlanningsTool.BLL/Mapper.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using PlanningsTool.Common.DTO.Zorgkundigen;
+using PlanningsTool.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanningsTool.BLL
+{
+    public class Mapper : Profile
+    {
+        public Mapper()
+        {
+            CreateMap<Zorgkundige, ZorgkundigeDTO>().ReverseMap();
+            CreateMap<Zorgkundige, ZorgkundigeDetailDTO>().ReverseMap();
+            CreateMap<Zorgkundige, CreateZorgkundigeDTO>().ReverseMap();
+            CreateMap<Zorgkundige, UpdateZorgkundigeDTO>().ReverseMap();
+        }
+    }
+}
