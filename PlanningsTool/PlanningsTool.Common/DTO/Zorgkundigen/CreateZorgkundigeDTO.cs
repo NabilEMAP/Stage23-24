cd234089dd37934dc65c9c4940f980a2a98759bc
NabilEMAP <Nabil_EM@outlook.com>
2023-10-12 15:17:36 +0200

    Added Zorgkundige Data Transfer Objects
    Added Zorgkundige DTO's in PlanningsTool.Common

---
+++ PlanningsTool/PlanningsTool.Common/DTO/Zorgkundigen/CreateZorgkundigeDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanningsTool.Common.DTO.Zorgkundigen
+{
+    public class CreateZorgkundigeDTO
+    {
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public bool IsVasteNacht { get; set; }
+    }
+}
---
+++ PlanningsTool/PlanningsTool.Common/DTO/Zorgkundigen/ZorgkundigeDetailDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanningsTool.Common.DTO.Zorgkundigen
+{
+    public class ZorgkundigeDetailDTO
+    {
+        public int Id { get; set; }
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public bool IsVasteNacht { get; set; }
+    }
+}
