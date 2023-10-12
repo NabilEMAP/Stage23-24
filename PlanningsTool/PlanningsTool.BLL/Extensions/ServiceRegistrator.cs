e8a77898896e4e8aeccb9ddbdb67eb71057a2ebc
NabilEMAP <Nabil_EM@outlook.com>
2023-10-12 10:19:27 +0200

    Added Service Registrators
    Added DAL and BLL Service Registrators and grouping it into the Program.cs API-Layer

---
+++ PlanningsTool/PlanningsTool.BLL/Extensions/ServiceRegistrator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanningsTool.BLL.Extensions
+{
+    public static class ServiceRegistrator
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            // Service Scopes moeten hier nog toegevoegd worden
+            // ...
+            return services;
+        }
+
+        public static IServiceCollection RegisterApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
---
+++ PlanningsTool/PlanningsTool.DAL/Extensions/DALRegistrator.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanningsTool.DAL.Extensions
+{
+    public static class DALRegistrator
+    {
+        public static IServiceCollection RegisterDbContext(this IServiceCollection services)
+        {
+            // Service AddDbContext moet hier nog toegevoegd worden
+            // ...
+            return services;
+        }
+
+        public static IServiceCollection RegisterRepositories(this IServiceCollection services)
+        {
+            // Service Scopes moeten hier nog toegevoegd worden
+            // ...
+            return services;
+        }
+
+        public static IServiceCollection RegisterUnitOfWork(this IServiceCollection services)
+        {
+            // Service Scopes moeten hier nog toegevoegd worden
+            // ...
+            return services;
+        }
+
+        public static IServiceCollection RegisterInfrastructure(this IServiceCollection services)
+        {
+            services.RegisterDbContext();
+            services.RegisterRepositories();
+            services.RegisterUnitOfWork();
+            return services;
+        }
+    }
+}
