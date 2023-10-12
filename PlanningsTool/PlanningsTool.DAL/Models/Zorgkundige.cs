756b40cd25bf619cab3642393f7979366653ac3f
NabilEMAP <Nabil_EM@outlook.com>
2023-10-12 10:03:08 +0200

    Create Zorgkundige.cs in PlanningsTool.DAL.Models

---
+++ PlanningsTool/PlanningsTool.DAL/Models/Zorgkundige.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanningsTool.DAL.Models
+{
+    public class Zorgkundige
+    {
+        public int Id { get; set; }
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        //public Regime Regime { get; set; }
+        public bool? IsVasteNacht { get; set; }
+    }
+}
