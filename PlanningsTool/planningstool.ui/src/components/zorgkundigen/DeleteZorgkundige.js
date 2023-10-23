c9db4ad9bd09899a8e2060969273480eba662f86
NabilEMAP <Nabil_EM@outlook.com>
2023-10-23 16:25:30 +0200

    Added RegimeType in DeleteZorgkundige.js

--- PlanningsTool/planningstool.ui/src/components/zorgkundigen/DeleteZorgkundige.js
+++ PlanningsTool/planningstool.ui/src/components/zorgkundigen/DeleteZorgkundige.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Modal from 'react-bootstrap/Modal';
 import { toast } from 'react-toastify';
-import { Button, Checkbox, FormControlLabel, IconButton, Stack, TextField } from "@mui/material";
+import { Button, Checkbox, FormControl, FormControlLabel, IconButton, InputLabel, MenuItem, Select, Stack, TextField } from "@mui/material";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
@@ -14,18 +14,32 @@ function DeleteZorgkundige(props) {
   const [Id, editId] = useState('');
   const [voornaam, setVoornaam] = useState('');
   const [achternaam, setAchternaam] = useState('');
+  const [regimeType, setRegimeType] = useState('');
   const [isVasteNacht, setIsVasteNacht] = useState(false);
   const [data, setData] = useState([]);
+  const [regimeTypeData, setRegimeTypeData] = useState([]);
 
   useEffect(() => {
     getData();
-  }, [data]);
+    getRegimeTypeData();
+  }, []);
 
   const getData = () => {
     const API = 'https://localhost:8000/api/Zorgkundigen/details'
     axios.get(API)
       .then((result) => {
         setData(result.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  const getRegimeTypeData = () => {
+    const API = 'https://localhost:8000/api/RegimeTypes'
+    axios.get(API)
+      .then((result) => {
+        setRegimeTypeData(result.data);
       })
       .catch((error) => {
         console.log(error);
@@ -40,6 +54,7 @@ function DeleteZorgkundige(props) {
         setVoornaam(result.data.voornaam);
         setAchternaam(result.data.achternaam);
         setIsVasteNacht(result.data.isVasteNacht);
+        setRegimeType(result.data.regimeId);
         editId(id);
       })
       .catch((error) => {
@@ -68,6 +83,12 @@ function DeleteZorgkundige(props) {
     else {
       setIsVasteNacht(false);
     }
+  }
+
+  const renderRegimeType = () => {
+    return regimeTypeData.map((item) => (
+      <MenuItem value={item.id}>{item.regime}</MenuItem>      
+    ));
   }
 
   return (
@@ -109,6 +130,15 @@ function DeleteZorgkundige(props) {
               onChange={(e) => setAchternaam(e.target.value)}
               disabled
             />
+            <FormControl style={{ width: '75%' }} disabled>
+              <InputLabel>Selecteer uw regime...</InputLabel>
+              <Select
+                value={regimeType}
+                onChange={(e) => setRegimeType(e.target.value)}
+              >
+                {renderRegimeType()}
+              </Select>
+            </FormControl>
             <FormControlLabel label="Vaste Nacht" control={
               <Checkbox type="checkbox"
                 checked={isVasteNacht === true ? true : false}
