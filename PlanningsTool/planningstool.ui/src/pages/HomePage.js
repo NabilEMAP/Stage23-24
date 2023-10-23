ed4f717e3b8007fce0203494b0f55a5cfb857ba2
NabilEMAP <Nabil_EM@outlook.com>
2023-10-23 10:19:02 +0200

    Updated footer

--- PlanningsTool/planningstool.ui/src/pages/HomePage.js
+++ PlanningsTool/planningstool.ui/src/pages/HomePage.js
@@ -10,7 +10,6 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { homecards } from "../constants";
 
 function Copyright() {
@@ -19,86 +18,79 @@ function Copyright() {
             {'Copyright © '}
             <Link color="inherit" href="https://github.com/NabilEMAP/Stage23-24/">
                 Nabil El Moussaoui
-            </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
+            </Link>
+            {' 2023 - 2024.'}
         </Typography>
     );
 }
 
-// TODO remove, this demo shouldn't need to reset the theme.
-const defaultTheme = createTheme();
-
 function HomePage() {
     return (
         <Fragment>
-            <ThemeProvider theme={defaultTheme}>
-                <CssBaseline />
-                <main>
-                    {/* Hero unit */}
-                    <Box
-                        sx={{
-                            bgcolor: 'background.paper',
-                            pt: 8,
-                            pb: 6,
-                        }}
-                    >
-                        <Container maxWidth="sm">
-                            <Typography
-                                component="h1"
-                                variant="h2"
-                                align="center"
-                                color="text.primary"
-                                gutterBottom
-                            >
-                                Planningstool <span style={{ color: "#d10202" }}>voor</span> Ziekenhuisdienst
-                            </Typography>
-                            <Typography variant="h5" align="center" color="text.secondary" paragraph>
-                                Een planningstool is een softwaretoepassing of digitale tool die is ontworpen om de zorgkundigen hun activiteiten in te plannen.
-                            </Typography>
-                            <Stack
-                                sx={{ pt: 4 }}
-                                direction="row"
-                                spacing={2}
-                                justifyContent="center"
-                            >
-                                <Button href="/teamplanning" variant="contained" color="error">Maak een teamplanning</Button>
-                                {/*<Button href="/over" variant="outlined" color="error">Over</Button>*/}
-                            </Stack>
-                        </Container>
-                    </Box>
-                    <Container sx={{ py: 8 }} maxWidth="md">
-                        <Grid container spacing={4}>
-                            {homecards.map((homecard) => (
-                                <Grid item key={homecard} xs={12} sm={6} md={3}>
-                                    <a href={homecard.url} style={{ textDecoration: "none" }}>
-                                        <Card
-                                            sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                                        >
-                                            <CardMedia
-                                                component="div"
-                                                sx={{
-                                                    // 16:9
-                                                    pt: '56.25%',
-                                                }}
-                                                image={homecard.image}
-                                            />
-                                            <CardContent sx={{ flexGrow: 1 }}>
-                                                <Typography gutterBottom variant="h5" component="h2">
-                                                    {homecard.heading}
-                                                </Typography>
-                                            </CardContent>
-                                        </Card>
-                                    </a>
-                                </Grid>
-                            ))}
-                        </Grid>
+            <CssBaseline />
+            <main>
+                <Box
+                    sx={{
+                        bgcolor: 'background.paper',
+                        pt: 8,
+                        pb: 6,
+                    }}
+                >
+                    <Container maxWidth="sm">
+                        <Typography
+                            component="h1"
+                            variant="h2"
+                            align="center"
+                            color="text.primary"
+                            gutterBottom
+                        >
+                            Planningstool <span style={{ color: "#d10202" }}>voor</span> Ziekenhuisdienst
+                        </Typography>
+                        <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                            Een planningstool is een softwaretoepassing of digitale tool die is ontworpen om de zorgkundigen hun activiteiten in te plannen.
+                        </Typography>
+                        <Stack
+                            sx={{ pt: 4 }}
+                            direction="row"
+                            spacing={2}
+                            justifyContent="center"
+                        >
+                            <Button href="/teamplanning" variant="contained" color="error">Maak een teamplanning</Button>
+                            {/*<Button href="/over" variant="outlined" color="error">Over</Button>*/}
+                        </Stack>
                     </Container>
-                </main>
-                <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-                    <Copyright />
                 </Box>
-            </ThemeProvider>
+                <Container sx={{ py: 8 }} maxWidth="md">
+                    <Grid container spacing={4}>
+                        {homecards.map((homecard) => (
+                            <Grid item key={homecard} xs={12} sm={6} md={3}>
+                                <a href={homecard.url} style={{ textDecoration: "none" }}>
+                                    <Card
+                                        sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                                    >
+                                        <CardMedia
+                                            component="div"
+                                            sx={{
+                                                // 16:9
+                                                pt: '56.25%',
+                                            }}
+                                            image={homecard.image}
+                                        />
+                                        <CardContent sx={{ flexGrow: 1 }}>
+                                            <Typography gutterBottom variant="h5" component="h2">
+                                                {homecard.heading}
+                                            </Typography>
+                                        </CardContent>
+                                    </Card>
+                                </a>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Container>
+            </main>
+            <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+                <Copyright />
+            </Box>
         </Fragment>
     );
 }
