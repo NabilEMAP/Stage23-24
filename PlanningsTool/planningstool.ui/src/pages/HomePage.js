977f66c0ecb89aa23307fcef0c8f91e92231c439
NabilEMAP <Nabil_EM@outlook.com>
2023-10-23 09:02:04 +0200

    Added title, description, button and footer in HomePage.js

--- PlanningsTool/planningstool.ui/src/pages/HomePage.js
+++ PlanningsTool/planningstool.ui/src/pages/HomePage.js
@@ -1,4 +1,93 @@
-function HomePage(params) {
-    
+import React, { Fragment } from "react";
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://github.com/NabilEMAP/Stage23-24/">
+                Nabil El Moussaoui
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+function HomePage() {
+    return (
+        <Fragment>
+            <ThemeProvider theme={defaultTheme}>
+                <CssBaseline />
+                <main>
+                    {/* Hero unit */}
+                    <Box
+                        sx={{
+                            bgcolor: 'background.paper',
+                            pt: 8,
+                            pb: 6,
+                        }}
+                    >
+                        <Container maxWidth="sm">
+                            <Typography
+                                component="h1"
+                                variant="h2"
+                                align="center"
+                                color="text.primary"
+                                gutterBottom
+                            >
+                                Planningstool <span style={{ color: "#d10202" }}>voor</span> Ziekenhuisdienst
+                            </Typography>
+                            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                            Een planningstool is een softwaretoepassing of digitale tool die is ontworpen om de zorgkundigen hun activiteiten in te plannen. 
+                            </Typography>
+                            <Stack
+                                sx={{ pt: 4 }}
+                                direction="row"
+                                spacing={2}
+                                justifyContent="center"
+                            >
+                                <Button href="/teamplanning" variant="contained" color="error">Maak een teamplanning</Button>
+                                
+                            </Stack>
+                        </Container>
+                    </Box>
+                    <Container sx={{ py: 8 }} maxWidth="md">
+                        {/* End hero unit */}
+                        <Grid container spacing={4}>
+                            {/* Cards */}
+                        </Grid>
+                    </Container>
+                </main>
+                {/* Footer */}
+                <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+                    <Typography variant="h6" align="center" gutterBottom>
+                        Footer
+                    </Typography>
+                    <Typography
+                        variant="subtitle1"
+                        align="center"
+                        color="text.secondary"
+                        component="p"
+                    >
+                        Something here to give the footer a purpose!
+                    </Typography>
+                    <Copyright />
+                </Box>
+                {/* End footer */}
+            </ThemeProvider>
+        </Fragment>
+    );
 }
 export default HomePage;
