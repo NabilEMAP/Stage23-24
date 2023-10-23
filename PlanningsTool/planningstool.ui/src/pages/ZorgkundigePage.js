2bfba17d62add53c28ea3c9764b6971c4b6f1731
NabilEMAP <Nabil_EM@outlook.com>
2023-10-23 13:41:51 +0200

    Added CheckCircleIcon and CancelIcon for IsVasteNacht attribute

--- PlanningsTool/planningstool.ui/src/pages/ZorgkundigePage.js
+++ PlanningsTool/planningstool.ui/src/pages/ZorgkundigePage.js
@@ -12,6 +12,8 @@ import AddZorgkundige from "../components/zorgkundigen/AddZorgkundige";
 import EditZorgkundige from "../components/zorgkundigen/EditZorgkundige";
 import DeleteZorgkundige from "../components/zorgkundigen/DeleteZorgkundige";
 import { Container, Typography } from "@mui/material";
+import CancelIcon from '@mui/icons-material/Cancel';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 function ZorgkundigePage() {
     const [data, setData] = useState([]);
@@ -39,8 +41,10 @@ function ZorgkundigePage() {
                     <MyTC>{item.voornaam}</MyTC>
                     <MyTC>{item.achternaam}</MyTC>
                     <MyTC>{item.regimeType.regime}</MyTC>
-                    <MyTC>{item.isVasteNacht ? "Ja" : "Nee"}</MyTC>
-                    <MyTC style={{width: '150px'}}>
+                    <MyTC>
+                        {item.isVasteNacht ? <CheckCircleIcon color="success" /> : <CancelIcon color="error" />}
+                    </MyTC>
+                    <MyTC style={{ width: '150px' }}>
                         <EditZorgkundige id={item.id} />
                         <DeleteZorgkundige id={item.id} />
                     </MyTC>
@@ -54,8 +58,8 @@ function ZorgkundigePage() {
     return (
         <Fragment>
             <Container>
-                <div style={{margin: '24px 0px'}}>
-                    <Typography variant="h5" style={{width: 'fit-content', verticalAlign: 'sub', display:'inline-block'}} >Zorgkundige Lijst</Typography>
+                <div style={{ margin: '24px 0px' }}>
+                    <Typography variant="h5" style={{ width: 'fit-content', verticalAlign: 'sub', display: 'inline-block' }} >Zorgkundige Lijst</Typography>
                     <AddZorgkundige />
                 </div>
                 <TableContainer component={Paper}>
@@ -67,7 +71,7 @@ function ZorgkundigePage() {
                                 <MyTC>Achternaam</MyTC>
                                 <MyTC>Regime</MyTC>
                                 <MyTC>Vaste Nacht?</MyTC>
-                                <MyTC style={{width: '150px'}}>Veranderingen</MyTC>
+                                <MyTC style={{ width: '150px' }}>Veranderingen</MyTC>
                             </TableRow>
                         </TableHead>
                         <TableBody>
