65d91ea487307a28aed3777690fcbc970ef6efae
NabilEMAP <Nabil_EM@outlook.com>
2023-10-24 09:58:12 +0200

    Added Read VerlofAndShiftType frontend

--- PlanningsTool/planningstool.ui/src/pages/types/VerlofTypePage.js
+++ PlanningsTool/planningstool.ui/src/pages/types/VerlofTypePage.js
@@ -1,4 +1,68 @@
+import axios from "axios";
+import React, { Fragment, useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { MyTC, MyTR } from "../../components/MyTable";
+import { Container, Typography } from "@mui/material";
+
 function VerlofTypePage() {
-    
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        getData();
+    }, [data]);
+
+    const getData = () => {
+        const API = 'https://localhost:8000/api/VerlofTypes'
+        axios.get(API)
+            .then((result) => {
+                setData(result.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    const renderTableData = () => {
+        if (data && data.length > 0) {
+            return data.map((item, index) => (
+                <MyTR key={index}>
+                    <MyTC>{item.id}</MyTC>
+                    <MyTC>{item.verlof}</MyTC>
+                </MyTR>
+            ));
+        } else {
+            return <TableRow><MyTC colSpan={5}>No data found</MyTC></TableRow>;
+        }
+    }
+
+    return (
+        <Fragment>
+            <Container>
+                <div style={{ margin: '24px 0px' }}>
+                    <Typography variant="h5" style={{ width: 'fit-content', verticalAlign: 'sub', display: 'inline-block' }} >Verlof Lijst</Typography>
+                </div>
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                        <TableHead>
+                            <TableRow>
+                                <MyTC>Id</MyTC>
+                                <MyTC>Verlof</MyTC>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {renderTableData()}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Container>
+        </Fragment>
+    );
 }
+
 export default VerlofTypePage;
