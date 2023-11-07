fbce8e98dde59ce031c5c537e3e96875c0f28829
NabilEMAP <Nabil_EM@outlook.com>
2023-11-07 13:54:02 +0100

    Added Vacations Validation

---
+++ PlanningsTool/PlanningsTool.BLL/Validations/CreateVacationValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using PlanningsTool.Common.DTO.Vacations;
+
+namespace PlanningsTool.BLL.Validations
+{
+    public class CreateVacationValidator : AbstractValidator<CreateVacationDTO>
+    {
+        public CreateVacationValidator()
+        {
+            RuleFor(z => z.Startdate)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Startdatum mag niet leeg zijn");
+
+            RuleFor(z => z.Enddate)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Einddatum mag niet leeg zijn")
+                .GreaterThanOrEqualTo(z => z.Startdate)
+                .WithMessage("Einddatum mag niet eerder zijn dan Startdatum");
+        }
+    }
+}
