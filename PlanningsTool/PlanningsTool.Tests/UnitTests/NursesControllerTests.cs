d3cc04ba56e902ce7b29dd2a0af8a8e46b8ab041
NabilEMAP <Nabil_EM@outlook.com>
2023-11-30 21:15:53 +0100

    Added TestInitialize Setup and some more Unit tests

--- PlanningsTool/PlanningsTool.Tests/UnitTests/NursesControllerTests.cs
+++ PlanningsTool/PlanningsTool.Tests/UnitTests/NursesControllerTests.cs
@@ -13,7 +13,16 @@ namespace PlanningsTool.Tests.UnitTests
     [TestClass]
     public class NursesControllerTests
     {
-        // Setup
+        private NursesController _controller;
+        private Mock<INursesService> _nursesServiceMock;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _nursesServiceMock = new Mock<INursesService>();
+            _controller = new NursesController(_nursesServiceMock.Object);
+        }
+
         private IEnumerable<NurseDTO> GetTestNurses()
         {
             return new List<NurseDTO>
@@ -28,129 +37,137 @@ namespace PlanningsTool.Tests.UnitTests
         public async Task GetAll_ReturnsOkResultWithNurses()
         {
             // Arrange
-            var nursesServiceMock = new Mock<INursesService>();
-            nursesServiceMock.Setup(repo => repo.GetAll()).ReturnsAsync(GetTestNurses());
-
-            var controller = new NursesController(nursesServiceMock.Object);
+            var testNurses = GetTestNurses();
+            _nursesServiceMock.Setup(repo => repo.GetAll()).ReturnsAsync(testNurses);
 
             // Act
-            var result = await controller.GetAll();
+            var result = await _controller.GetAll();
 
             // Assert
-            Assert.IsNotNull(result);
             Assert.IsInstanceOfType(result, typeof(OkObjectResult));
-
             var okResult = (OkObjectResult)result;
             var nurses = (IEnumerable<NurseDTO>)okResult.Value;
-            Assert.IsNotNull(nurses);
-            // Add more specific assertions based on your business logic
+            CollectionAssert.AreEqual(testNurses.ToList(), nurses.ToList());
         }
 
         [TestMethod]
         public async Task GetById_ReturnsOkResultWithNurse()
         {
             // Arrange
-            var nursesServiceMock = new Mock<INursesService>();
-            nursesServiceMock.Setup(repo => repo.GetById(It.IsAny<int>())).ReturnsAsync(new NurseDTO { Id = 1, FirstName = "John", LastName = "Doe", RegimeTypeId = 1, IsFixedNight = false });
-
-            var controller = new NursesController(nursesServiceMock.Object);
+            var testNurse = GetTestNurses().FirstOrDefault();
+            _nursesServiceMock.Setup(repo => repo.GetById(It.IsAny<int>())).ReturnsAsync(testNurse);
 
             // Act
-            var result = await controller.GetById(1);
+            var result = await _controller.GetById(1);
 
             // Assert
-            Assert.IsNotNull(result);
             Assert.IsInstanceOfType(result, typeof(OkObjectResult));
-
             var okResult = (OkObjectResult)result;
             var nurse = (NurseDTO)okResult.Value;
-            Assert.IsNotNull(nurse);
-            Assert.AreEqual(1, nurse.Id);
-            // Add more specific assertions based on your business logic
+            Assert.AreEqual(testNurse, nurse);
         }
 
         [TestMethod]
         public async Task GetNursesByFirstName_ReturnsOkResultWithNurses()
         {
             // Arrange
-            var nursesServiceMock = new Mock<INursesService>();
-            nursesServiceMock.Setup(repo => repo.GetNursesByFirstName(It.IsAny<string>())).ReturnsAsync(GetTestNurses());
-
-            var controller = new NursesController(nursesServiceMock.Object);
+            var testNurses = GetTestNurses();
+            _nursesServiceMock.Setup(repo => repo.GetNursesByFirstName(It.IsAny<string>())).ReturnsAsync(testNurses);
 
             // Act
-            var result = await controller.GetNursesByFirstName("John");
+            var result = await _controller.GetNursesByFirstName("John");
 
             // Assert
-            Assert.IsNotNull(result);
             Assert.IsInstanceOfType(result, typeof(OkObjectResult));
-
             var okResult = (OkObjectResult)result;
             var nurses = (IEnumerable<NurseDTO>)okResult.Value;
-            Assert.IsNotNull(nurses);
-            // Add more specific assertions based on your business logic
+            CollectionAssert.AreEqual(testNurses.ToList(), nurses.ToList());
         }
 
         [TestMethod]
-        public async Task Post_ReturnsOkResultWithNurse()
+        public async Task GetNursesByLastName_ReturnsOkResultWithNurses()
         {
             // Arrange
-            var nursesServiceMock = new Mock<INursesService>();
-            nursesServiceMock.Setup(repo => repo.Add(It.IsAny<CreateNurseDTO>())).ReturnsAsync(new NurseDTO { Id = 1, FirstName = "John", LastName = "Doe" });
-
-            var controller = new NursesController(nursesServiceMock.Object);
+            var testNurses = GetTestNurses();
+            _nursesServiceMock.Setup(repo => repo.GetNursesByLastName(It.IsAny<string>())).ReturnsAsync(testNurses);
 
             // Act
-            var result = await controller.Post(new CreateNurseDTO { FirstName = "John", LastName = "Doe" });
+            var result = await _controller.GetNursesByLastName("Doe");
 
             // Assert
-            Assert.IsNotNull(result);
             Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = (OkObjectResult)result;
+            var nurses = (IEnumerable<NurseDTO>)okResult.Value;
+            CollectionAssert.AreEqual(testNurses.ToList(), nurses.ToList());
+        }
 
+        [TestMethod]
+        public async Task Post_ReturnsOkResultWithCreatedNurse()
+        {
+            // Arrange
+            var createNurseDTO = new CreateNurseDTO { FirstName = "John", LastName = "Doe", RegimeTypeId = 1, IsFixedNight = false };
+            var createdNurse = new NurseDTO { Id = 1, FirstName = "John", LastName = "Doe", RegimeTypeId = 1, IsFixedNight = false };
+
+            _nursesServiceMock.Setup(repo => repo.Add(It.IsAny<CreateNurseDTO>())).ReturnsAsync(createdNurse);
+
+            // Act
+            var result = await _controller.Post(createNurseDTO);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
             var okResult = (OkObjectResult)result;
             var nurse = (CreateNurseDTO)okResult.Value;
-            Assert.IsNotNull(nurse);
-            // Add more specific assertions based on your business logic
+
+            // Assert the properties of the returned object
+            Assert.AreEqual(createNurseDTO.FirstName, nurse.FirstName);
+            Assert.AreEqual(createNurseDTO.LastName, nurse.LastName);
+            Assert.AreEqual(createNurseDTO.RegimeTypeId, nurse.RegimeTypeId);
+            Assert.AreEqual(createNurseDTO.IsFixedNight, nurse.IsFixedNight);
         }
+
+
 
 
         [TestMethod]
         public async Task Put_ReturnsOkResultWithUpdatedNurse()
         {
             // Arrange
-            var nursesServiceMock = new Mock<INursesService>();
-            nursesServiceMock.Setup(repo => repo.Update(It.IsAny<int>(), It.IsAny<UpdateNurseDTO>())).ReturnsAsync(new NurseDTO { Id = 1, FirstName = "UpdatedJohn", LastName = "UpdatedDoe" });
+            int nurseId = 1;
+            var updateNurseDTO = new UpdateNurseDTO { FirstName = "UpdatedFirstName", LastName = "UpdatedLastName", RegimeTypeId = 2, IsFixedNight = true };
+            var existingNurse = new NurseDTO { Id = nurseId, FirstName = "John", LastName = "Doe", RegimeTypeId = 1, IsFixedNight = false };
+            var updatedNurse = new NurseDTO { Id = nurseId, FirstName = "UpdatedFirstName", LastName = "UpdatedLastName", RegimeTypeId = 2, IsFixedNight = true };
 
-            var controller = new NursesController(nursesServiceMock.Object);
+            _nursesServiceMock.Setup(repo => repo.Update(nurseId, updateNurseDTO)).ReturnsAsync(updatedNurse);
+            _nursesServiceMock.Setup(repo => repo.GetById(nurseId)).ReturnsAsync(existingNurse);
 
             // Act
-            var result = await controller.Put(1, new UpdateNurseDTO { FirstName = "UpdatedJohn", LastName = "UpdatedDoe" });
+            var result = await _controller.Put(nurseId, updateNurseDTO);
 
             // Assert
-            Assert.IsNotNull(result);
             Assert.IsInstanceOfType(result, typeof(OkObjectResult));
-
             var okResult = (OkObjectResult)result;
-            var updatedNurse = (UpdateNurseDTO)okResult.Value;
-            Assert.IsNotNull(updatedNurse);
-            // Add more specific assertions based on your business logic
+            var nurse = (UpdateNurseDTO)okResult.Value;
+
+            // Assert the properties of the returned object
+            Assert.AreEqual(updateNurseDTO.FirstName, nurse.FirstName);
+            Assert.AreEqual(updateNurseDTO.LastName, nurse.LastName);
+            Assert.AreEqual(updateNurseDTO.RegimeTypeId, nurse.RegimeTypeId);
+            Assert.AreEqual(updateNurseDTO.IsFixedNight, nurse.IsFixedNight);
         }
+
 
         [TestMethod]
         public async Task Delete_ReturnsNoContentResult()
         {
             // Arrange
-            var nursesServiceMock = new Mock<INursesService>();
-            nursesServiceMock.Setup(repo => repo.Delete(It.IsAny<int>())).ReturnsAsync(0);
-
-            var controller = new NursesController(nursesServiceMock.Object);
+            var testNurseId = 1;
+            _nursesServiceMock.Setup(repo => repo.GetById(testNurseId)).ReturnsAsync(GetTestNurses().FirstOrDefault());
 
             // Act
-            var result = await controller.Delete(1);
+            var result = await _controller.Delete(testNurseId);
 
             // Assert
-            Assert.IsNotNull(result);
-            Assert.IsInstanceOfType(result, typeof(NoContentResult)); //Error
+            Assert.IsInstanceOfType(result, typeof(NoContentResult));
         }
 
     }
---
+++ PlanningsTool/PlanningsTool.Tests/UnitTests/VacationsControllerTests.cs
@@ -0,0 +1,220 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PlanningsTool.API.Controllers;
+using PlanningsTool.BLL.Interfaces;
+using PlanningsTool.Common.DTO.Vacations;
+using PlanningsTool.DAL.Models;
+
+namespace PlanningsTool.Tests.UnitTests
+{
+    [TestClass]
+    public class VacationsControllerTests
+    {
+        private VacationsController _controller;
+        private Mock<IVacationsService> _vacationsServiceMock;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _vacationsServiceMock = new Mock<IVacationsService>();
+            _controller = new VacationsController(_vacationsServiceMock.Object);
+        }
+
+        private IEnumerable<VacationDetailDTO> GetTestVacations()
+        {
+            return new List<VacationDetailDTO>
+            {
+                new VacationDetailDTO
+                {
+                    Id = 1,
+                    Startdate = new DateTime(2023, 9, 15),
+                    Enddate = new DateTime(2023, 9, 15),
+                    Reason = "Verlofdagje op vrijdag.",
+                    NurseId = 1,
+                    VacationTypeId = 1
+                },
+                new VacationDetailDTO
+                {
+                    Id = 2,
+                    Startdate = new DateTime(2023, 9, 18),
+                    Enddate = new DateTime(2023, 9, 24),
+                    Reason = "Heel de week ziek geweest.",
+                    NurseId = 2,
+                    VacationTypeId = 2
+                },
+                new VacationDetailDTO
+                {
+                    Id = 3,
+                    Startdate = new DateTime(2023, 9, 24),
+                    Enddate = new DateTime(2023, 9, 18),
+                    Reason = "Ik ga eens terug in de tijd :D, eens zien of ik een error kan geven.",
+                    NurseId = 3,
+                    VacationTypeId = 5
+                },
+            };
+        }
+
+        [TestMethod]
+        public async Task GetAll_ReturnsOkResultWithVacations()
+        {
+            // Arrange
+            var testVacations = GetTestVacations();
+            _vacationsServiceMock.Setup(repo => repo.GetAllDetails()).ReturnsAsync(testVacations);
+
+            // Act
+            var result = await _controller.GetAllDetails();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = (OkObjectResult)result;
+            var vacations = (IEnumerable<VacationDetailDTO>)okResult.Value;
+            CollectionAssert.AreEqual(testVacations.ToList(), vacations.ToList());
+        }
+
+        [TestMethod]
+        public async Task GetById_ReturnsOkResultWithVacation()
+        {
+            // Arrange
+            var testVacation = GetTestVacations().FirstOrDefault();
+            _vacationsServiceMock.Setup(repo => repo.GetById(It.IsAny<int>())).ReturnsAsync(testVacation);
+
+            // Act
+            var result = await _controller.GetById(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = (OkObjectResult)result;
+            var vacation = (VacationDetailDTO)okResult.Value;
+            Assert.AreEqual(testVacation, vacation);
+        }
+
+        [TestMethod]
+        public async Task GetVacationsByReason_ReturnsOkResultWithVacations()
+        {
+            // Arrange
+            var testVacations = GetTestVacations();
+            _vacationsServiceMock.Setup(repo => repo.GetVacationsByReason(It.IsAny<string>())).ReturnsAsync(testVacations);
+
+            // Act
+            var result = await _controller.GetVacationsByReason("Verlofdagje op vrijdag.");
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = (OkObjectResult)result;
+            var vacations = (IEnumerable<VacationDetailDTO>)okResult.Value;
+            CollectionAssert.AreEqual(testVacations.ToList(), vacations.ToList());
+        }
+
+        [TestMethod]
+        public async Task Post_ReturnsOkResultWithCreatedVacation()
+        {
+            // Arrange
+            var createVacationDTO = new CreateVacationDTO 
+            {
+                Startdate = new DateTime(2023, 9, 15),
+                Enddate = new DateTime(2023, 9, 15),
+                Reason = "Verlofdagje op vrijdag.",
+                NurseId = 1,
+                VacationTypeId = 1
+            };
+            var createdVacation = new VacationDetailDTO 
+            {
+                Id = 1,
+                Startdate = new DateTime(2023, 9, 15),
+                Enddate = new DateTime(2023, 9, 15),
+                Reason = "Verlofdagje op vrijdag.",
+                NurseId = 1,
+                VacationTypeId = 1
+            };
+
+            _vacationsServiceMock.Setup(repo => repo.Add(It.IsAny<CreateVacationDTO>())).ReturnsAsync(createdVacation);
+
+            // Act
+            var result = await _controller.Post(createVacationDTO);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = (OkObjectResult)result;
+            var vacation = (CreateVacationDTO)okResult.Value;
+
+            // Assert the properties of the returned object
+            Assert.AreEqual(createVacationDTO.Startdate, vacation.Startdate);
+            Assert.AreEqual(createVacationDTO.Enddate, vacation.Enddate);
+            Assert.AreEqual(createVacationDTO.Reason, vacation.Reason);
+            Assert.AreEqual(createVacationDTO.NurseId, vacation.NurseId);
+            Assert.AreEqual(createVacationDTO.VacationTypeId, vacation.VacationTypeId);
+        }
+
+        [TestMethod]
+        public async Task Put_ReturnsOkResultWithUpdatedVacation()
+        {
+            // Arrange
+            int vacationId = 1;
+            var updateVacationDTO = new UpdateVacationDTO 
+            {
+                Startdate = new DateTime(2023, 9, 18),
+                Enddate = new DateTime(2023, 9, 18),
+                Reason = "Verlofdagje op maandag.",
+                NurseId = 1,
+                VacationTypeId = 1
+            };
+            var existingVacation = new VacationDetailDTO 
+            { 
+                Id = vacationId,
+                Startdate = new DateTime(2023, 9, 15),
+                Enddate = new DateTime(2023, 9, 15),
+                Reason = "Verlofdagje op vrijdag.",
+                NurseId = 1,
+                VacationTypeId = 1
+            };
+            var updatedVacation = new VacationDetailDTO 
+            { 
+                Id = vacationId,
+                Startdate = new DateTime(2023, 9, 18),
+                Enddate = new DateTime(2023, 9, 18),
+                Reason = "Verlofdagje op maandag.",
+                NurseId = 1,
+                VacationTypeId = 1
+            };
+
+            _vacationsServiceMock.Setup(repo => repo.Update(vacationId, updateVacationDTO)).ReturnsAsync(updatedVacation);
+            _vacationsServiceMock.Setup(repo => repo.GetById(vacationId)).ReturnsAsync(existingVacation);
+
+            // Act
+            var result = await _controller.Put(vacationId, updateVacationDTO);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = (OkObjectResult)result;
+            var vacation = (UpdateVacationDTO)okResult.Value;
+
+            // Assert the properties of the returned object
+            Assert.AreEqual(updateVacationDTO.Startdate, vacation.Startdate);
+            Assert.AreEqual(updateVacationDTO.Enddate, vacation.Enddate);
+            Assert.AreEqual(updateVacationDTO.Reason, vacation.Reason);
+            Assert.AreEqual(updateVacationDTO.NurseId, vacation.NurseId);
+            Assert.AreEqual(updateVacationDTO.VacationTypeId, vacation.VacationTypeId);
+        }
+
+
+        [TestMethod]
+        public async Task Delete_ReturnsNoContentResult()
+        {
+            // Arrange
+            var testVacationId = 1;
+            _vacationsServiceMock.Setup(repo => repo.GetById(testVacationId)).ReturnsAsync(GetTestVacations().FirstOrDefault());
+
+            // Act
+            var result = await _controller.Delete(testVacationId);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(NoContentResult));
+        }
+
+
+
+    }
+}
