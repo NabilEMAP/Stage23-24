c277b18f67081c21ee5c6e643c35106d79adf6a9
NabilEMAP <Nabil_EM@outlook.com>
2023-11-30 21:38:09 +0100

    Added my first Selenium Test

---
+++ PlanningsTool/PlanningsTool.Tests/SeleniumTests/SeleniumTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assert = NUnit.Framework.Assert;
+
+namespace PlanningsTool.Tests.SeleniumTests
+{
+    public class SeleniumTests
+    {
+        [Test]
+        public void GetTitleHomePage()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:3000/");
+
+            Assert.That(driver.Title, Is.EqualTo("React App"));
+
+            driver.Quit();
+        }
+    }
+}
