135ccf1a282f0d3a35dad33a707debbe53c747bd
NabilEMAP <Nabil_EM@outlook.com>
2023-12-05 15:35:12 +0100

    Added VacationCRUD selenium tests

--- PlanningsTool/PlanningsTool.Tests/SeleniumTests/NurseTests/NurseCRUD.cs
+++ PlanningsTool/PlanningsTool.Tests/SeleniumTests/NurseTests/NurseCRUD.cs
@@ -16,9 +16,11 @@ namespace PlanningsTool.Tests.SeleniumTests.NurseTests
         private IWebDriver _driver;
         private string _URL;
         private string _firstName;
-        private string _updatedFirstName;
         private string _lastName;
+        private string _regime;
+        private string _updatedFirstName;
         private string _updatedLastName;
+        private string _updatedRegime;
 
         [TestInitialize]
         public void Setup()
@@ -26,32 +28,47 @@ namespace PlanningsTool.Tests.SeleniumTests.NurseTests
             _driver = new ChromeDriver();
             _URL = "http://localhost:3000/zorgkundige";
             _firstName = "TestFirstname";
-            _updatedFirstName = "TestUpdateFirstname";
             _lastName = "TestLastname";
+            _regime = "Voltijds";
+            _updatedFirstName = "TestUpdateFirstname";
             _updatedLastName = "TestUpdateLastname";
+            _updatedRegime = "Deeltijds 4/5";
         }
 
         [TestMethod]
         public void NurseCRUD_ST01_CreateNurse()
         {
-            string regime = "Voltijds";
             _driver.Navigate().GoToUrl(_URL);
+
+            // Addressing first record
             var sortByNew = _driver.FindElement(By.XPath("//div[contains(text(),'Id')]"));
             sortByNew.Click(); sortByNew.Click();
+
+            // Making a new nurse
             var createNurse = _driver.FindElement(By.Id("createNurse"));
             createNurse.Click();
+
+            // Inserting firstname and lastname
             var txtInputFirstname = _driver.FindElement(By.Id("txtInputFirstname"));
-            var txtInputLastname = _driver.FindElement(By.Id("txtInputLastname"));
-            var selectRegime = _driver.FindElement(By.Id("selectRegime"));
             txtInputFirstname.SendKeys(_firstName);
+            var txtInputLastname = _driver.FindElement(By.Id("txtInputLastname"));
             txtInputLastname.SendKeys(_lastName);
+
+            // Selecting regime
+            var selectRegime = _driver.FindElement(By.Id("selectRegime"));
             selectRegime.Click();
-            var selectSpecificRegime = _driver.FindElement(By.XPath($"//li[normalize-space()='{regime}']"));
+            var selectSpecificRegime = _driver.FindElement(By.XPath($"//li[normalize-space()='{_regime}']"));
             selectSpecificRegime.Click();
+
+            // Added fixednight
             var fixedNightInput = _driver.FindElement(By.Id("fixedNightInput"));
             fixedNightInput.Click();
+
+            // Added nurse
             var submitForm = _driver.FindElement(By.Id("submitNurseForm"));
             submitForm.Click();
+
+            // Driver quit
             _driver.Quit();
         }
 
@@ -59,15 +76,22 @@ namespace PlanningsTool.Tests.SeleniumTests.NurseTests
         public void NurseCRUD_ST02_ReadNurse()
         {
             _driver.Navigate().GoToUrl(_URL);
+
+            // Addressing first record
             var sortByNew = _driver.FindElement(By.XPath("//div[contains(text(),'Id')]"));
-            sortByNew.Click();
-            sortByNew.Click();
-            var readNurse = _driver.FindElement(By.XPath("//div[1]//div[6]//div[1]//button[1]//*[name()='svg']//*[name()='path' and contains(@fill,'currentCol')]"));
-            readNurse.Click();
+            sortByNew.Click(); sortByNew.Click();
+
+            // Reading in update modal
+            var read = _driver.FindElement(By.XPath("//div[1]//div[6]//div[1]//button[1]//*[name()='svg']//*[name()='path' and contains(@fill,'currentCol')]"));
+            read.Click();
+
+            // Read firstname and lastname
             var readFirstname = _driver.FindElement(By.XPath("//input[@id='txtInputFirstname']")).GetAttribute("value");
             var readLastname = _driver.FindElement(By.XPath("//input[@id='txtInputLastname']")).GetAttribute("value");
             Assert.AreEqual(_firstName, readFirstname);
             Assert.AreEqual(_lastName, readLastname);
+
+            // Driver quit
             _driver.Quit();
         }
 
@@ -76,26 +100,39 @@ namespace PlanningsTool.Tests.SeleniumTests.NurseTests
         public void NurseCRUD_ST03_UpdateNurse()
         {
             var clear = Keys.Control + "A" + Keys.Backspace;
-            string regime = "Deeltijds 4/5";
             _driver.Navigate().GoToUrl(_URL);
+
+            // Addressing first record
             var sortByNew = _driver.FindElement(By.XPath("//div[contains(text(),'Id')]"));
             sortByNew.Click(); sortByNew.Click();
+
+            // Update first record
             var updateNurse = _driver.FindElement(By.XPath("//div[1]//div[6]//div[1]//button[1]//*[name()='svg']//*[name()='path' and contains(@fill,'currentCol')]"));
             updateNurse.Click();
+
+            // Updating firstname and lastname
             var txtInputFirstname = _driver.FindElement(By.Id("txtInputFirstname"));
-            var txtInputLastname = _driver.FindElement(By.Id("txtInputLastname"));
-            var selectRegime = _driver.FindElement(By.Id("selectRegime"));
             txtInputFirstname.SendKeys(clear);
             txtInputFirstname.SendKeys(_updatedFirstName);
+            var txtInputLastname = _driver.FindElement(By.Id("txtInputLastname"));
             txtInputLastname.SendKeys(clear);
             txtInputLastname.SendKeys(_updatedLastName);
+
+            // Updating regime
+            var selectRegime = _driver.FindElement(By.Id("selectRegime"));
             selectRegime.Click();
-            var selectSpecificRegime = _driver.FindElement(By.XPath($"//li[normalize-space()='{regime}']"));
+            var selectSpecificRegime = _driver.FindElement(By.XPath($"//li[normalize-space()='{_updatedRegime}']"));
             selectSpecificRegime.Click();
+
+            // Updating fixednight
             var fixedNightInput = _driver.FindElement(By.Id("fixedNightInput"));
             fixedNightInput.Click();
+
+            // Update nurse
             var submitForm = _driver.FindElement(By.Id("submitNurseForm"));
             submitForm.Click();
+
+            // Driver quit
             _driver.Quit();
         }
 
@@ -103,15 +140,22 @@ namespace PlanningsTool.Tests.SeleniumTests.NurseTests
         public void NurseCRUD_ST04_ReadUpdatedNurse()
         {
             _driver.Navigate().GoToUrl(_URL);
+
+            // Addressing first record
             var sortByNew = _driver.FindElement(By.XPath("//div[contains(text(),'Id')]"));
-            sortByNew.Click();
-            sortByNew.Click();
+            sortByNew.Click(); sortByNew.Click();
+
+            // Read in update modal
             var readNurse = _driver.FindElement(By.XPath("//div[1]//div[6]//div[1]//button[1]//*[name()='svg']//*[name()='path' and contains(@fill,'currentCol')]"));
             readNurse.Click();
+
+            // Read firstname and lastname
             var readFirstname = _driver.FindElement(By.XPath("//input[@id='txtInputFirstname']")).GetAttribute("value");
             var readLastname = _driver.FindElement(By.XPath("//input[@id='txtInputLastname']")).GetAttribute("value");
             Assert.AreEqual(_updatedFirstName, readFirstname);
             Assert.AreEqual(_updatedLastName, readLastname);
+
+            // Driver quit
             _driver.Quit();
         }
 
@@ -119,12 +163,20 @@ namespace PlanningsTool.Tests.SeleniumTests.NurseTests
         public void NurseCRUD_ST05_DeleteNurse()
         {
             _driver.Navigate().GoToUrl(_URL);
+
+            // Addressing first record
             var sortByNew = _driver.FindElement(By.XPath("//div[contains(text(),'Id')]"));
             sortByNew.Click(); sortByNew.Click();
+
+            // Delete first record
             var deleteNurse = _driver.FindElement(By.XPath("//div[1]//div[6]//div[1]//button[2]//*[name()='svg']//*[name()='path' and contains(@fill,'currentCol')]"));
             deleteNurse.Click();
+
+            // Deleted nurse
             var submitForm = _driver.FindElement(By.Id("submitNurseForm"));
             submitForm.Click();
+
+            // Driver quit
             _driver.Quit();
         }
     }
--- PlanningsTool/PlanningsTool.Tests/SeleniumTests/VacationTests/VacationCRUD.cs
+++ PlanningsTool/PlanningsTool.Tests/SeleniumTests/VacationTests/VacationCRUD.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace PlanningsTool.Tests.SeleniumTests.VacationTests
 {
@@ -18,6 +19,7 @@ namespace PlanningsTool.Tests.SeleniumTests.VacationTests
         private string _enddate;
         private string _vacation;
         private string _reason;
+        private string _updatedNurse;
         private string _updatedStartDate;
         private string _updatedEnddate;
         private string _updatedVacation;
@@ -29,12 +31,13 @@ namespace PlanningsTool.Tests.SeleniumTests.VacationTests
             _driver = new ChromeDriver();
             _URL = "http://localhost:3000/verlof";
             _nurse = "Fatima Tsridh";
-            _startDate = "20042004";
-            _enddate = "21042004";
+            _startDate = "20/04/2004";
+            _enddate = "21/04/2004";
             _vacation = "Verlof";
             _reason = "Verlof test van 2 dagen";
-            _updatedStartDate = "01052004";
-            _updatedEnddate = "07052004";
+            _updatedNurse = "Fatima Tsridh";
+            _updatedStartDate = "01/05/2004";
+            _updatedEnddate = "07/05/2004";
             _updatedVacation = "Ziekte";
             _updatedReason = "Ziekte test van een week in mei";
         }
@@ -43,47 +46,179 @@ namespace PlanningsTool.Tests.SeleniumTests.VacationTests
         public void VacationCRUD_ST01_CreateVacation()
         {
             _driver.Navigate().GoToUrl(_URL);
-            
-            //Eerste Record aanspreken
+
+            // Addressing first record
             var sortByNew = _driver.FindElement(By.XPath("//div[contains(text(),'Id')]"));
             sortByNew.Click(); sortByNew.Click();
-           
-            //Nieuwe verlof aanmaken
+
+            // Making a new vacation
             var createVacation = _driver.FindElement(By.Id("createVacation"));
             createVacation.Click();
 
-            //Zorgkundige selecteren
+            // Selecting nurse
             var selectNurse = _driver.FindElement(By.Id("selectNurse"));
             selectNurse.Click();
             var selectSpecificNurse = _driver.FindElement(By.XPath($"//li[normalize-space()='{_nurse}']"));
             selectSpecificNurse.Click();
 
-            //Start en einddatum ingeven
-            var txtInputStartDate = _driver.FindElement(By.XPath("//input[@id=':r2p:']"));
+            // Inserting startdate and enddate
+            var txtInputStartDate = _driver.FindElement(By.XPath("//body//div[@role='dialog']//div//div//div//div[2]//div[1]//div[1]//input[1]"));
             txtInputStartDate.Click();
             txtInputStartDate.SendKeys(_startDate);
-            var txtInputEndDate = _driver.FindElement(By.XPath("//input[@id=':r2t:']"));
+            var txtInputEndDate = _driver.FindElement(By.XPath("//body/div[@role='dialog']/div/div/div/div/div[3]/div[1]/div[1]//input[1]"));
             txtInputEndDate.Click();
             txtInputEndDate.SendKeys(_enddate);
 
-            //Verlof selecteren
+            // Selecting vacation
             var selectVacation = _driver.FindElement(By.Id("selectVacation"));
             selectVacation.Click();
             var selectSpecificVacation = _driver.FindElement(By.XPath($"//li[normalize-space()='{_vacation}']"));
             selectSpecificVacation.Click();
 
-            //Reden ingeven
+            // Inserting reason
             var txtInputReason = _driver.FindElement(By.Id("txtInputReason"));
             txtInputReason.SendKeys(_reason);
 
-            //Verlof toevoegen
+            // Added vacation
             var submitForm = _driver.FindElement(By.Id("submitVacationForm"));
             submitForm.Click();
 
-            //Driver beeindigen
+            // Driver quit
             _driver.Quit();
         }
 
-        
+        [TestMethod]
+        public void VacationCRUD_ST02_ReadVacation()
+        {
+            _driver.Navigate().GoToUrl(_URL);
+
+            // Addressing first record
+            var sortByNew = _driver.FindElement(By.XPath("//div[contains(text(),'Id')]"));
+            sortByNew.Click(); sortByNew.Click();
+
+            // Reading in update modal
+            var read = _driver.FindElement(By.XPath("//body//div[@id='root']//div[@role='presentation']//div[@role='presentation']//div[@role='rowgroup']//div[1]//div[6]//button[1]//*[name()='svg']//*[name()='path' and contains(@fill,'currentCol')]"));
+            read.Click();
+
+            // Read nurse
+            //var readNurse = _driver.FindElement(By.XPath("//div[@id='selectNurse']")).GetAttribute("value");
+            //Assert.AreEqual(_nurse, readNurse);
+
+            // Read vacation
+            //var readVacation = _driver.FindElement(By.XPath("//div[@id='selectVacation']")).GetAttribute("value");
+            //Assert.AreEqual(_vacation, readVacation);
+
+            // Read startdate, enddate and reason
+            var readStartDate = _driver.FindElement(By.XPath("//body//div[@role='dialog']//div//div//div//div[2]//div[1]//div[1]//input[1]")).GetAttribute("value");
+            var readEndDate = _driver.FindElement(By.XPath("//body/div[@role='dialog']/div/div/div/div/div[3]/div[1]/div[1]//input[1]")).GetAttribute("value");
+            var readReason = _driver.FindElement(By.XPath("//textarea[@id='txtInputReason']")).GetAttribute("value");
+            Assert.AreEqual(_startDate, readStartDate);
+            Assert.AreEqual(_enddate, readEndDate);
+            Assert.AreEqual(_reason, readReason);
+
+            // Driver quit
+            _driver.Quit();
+        }
+
+
+        [TestMethod]
+        public void VacationCRUD_ST03_UpdateVacation()
+        {
+            var clear = Keys.Control + "A" + Keys.Backspace;
+            _driver.Navigate().GoToUrl(_URL);
+
+            // Addressing first record
+            var sortByNew = _driver.FindElement(By.XPath("//div[contains(text(),'Id')]"));
+            sortByNew.Click(); sortByNew.Click();
+
+            // Update first record
+            var updateVacation = _driver.FindElement(By.XPath("//body//div[@id='root']//div[@role='presentation']//div[@role='presentation']//div[@role='rowgroup']//div[1]//div[6]//button[1]//*[name()='svg']//*[name()='path' and contains(@fill,'currentCol')]"));
+            updateVacation.Click();
+
+            // Updating nurse
+            var selectNurse = _driver.FindElement(By.Id("selectNurse"));
+            selectNurse.Click();
+            var selectSpecificNurse = _driver.FindElement(By.XPath($"//li[normalize-space()='{_updatedNurse}']"));
+            selectSpecificNurse.Click();
+
+            // Updating startdate and enddate
+            var txtInputStartDate = _driver.FindElement(By.XPath("//body//div[@role='dialog']//div//div//div//div[2]//div[1]//div[1]//input[1]"));
+            txtInputStartDate.Click();
+            txtInputStartDate.SendKeys(clear);
+            txtInputStartDate.SendKeys(_updatedStartDate);
+            var txtInputEndDate = _driver.FindElement(By.XPath("//body/div[@role='dialog']/div/div/div/div/div[3]/div[1]/div[1]//input[1]"));
+            txtInputEndDate.Click();
+            txtInputEndDate.SendKeys(clear);
+            txtInputEndDate.SendKeys(_updatedEnddate);
+
+            // Updating vacation
+            var selectVacation = _driver.FindElement(By.Id("selectVacation"));
+            selectVacation.Click();
+            var selectSpecificVacation = _driver.FindElement(By.XPath($"//li[normalize-space()='{_updatedVacation}']"));
+            selectSpecificVacation.Click();
+
+            // Updating reason
+            var txtInputReason = _driver.FindElement(By.Id("txtInputReason"));
+            txtInputReason.SendKeys(clear);
+            txtInputReason.SendKeys(_updatedReason);
+
+            // Update vacation
+            var submitForm = _driver.FindElement(By.Id("submitVacationForm"));
+            submitForm.Click();
+
+            // Driver quit
+            _driver.Quit();
+        }
+
+        [TestMethod]
+        public void VacationCRUD_ST04_ReadUpdatedVacation()
+        {
+            _driver.Navigate().GoToUrl(_URL);
+
+            // Addressing first record
+            var sortByNew = _driver.FindElement(By.XPath("//div[contains(text(),'Id')]"));
+            sortByNew.Click(); sortByNew.Click();
+
+            // Read in update modal
+            var read = _driver.FindElement(By.XPath("//body//div[@id='root']//div[@role='presentation']//div[@role='presentation']//div[@role='rowgroup']//div[1]//div[6]//button[1]//*[name()='svg']//*[name()='path' and contains(@fill,'currentCol')]"));
+            read.Click();
+
+            // Read nurse
+            //var readNurse = _driver.FindElement(By.XPath("//div[@id='selectNurse']")).GetAttribute("value");
+            //Assert.AreEqual(_updatedNurse, readNurse);
+
+            // Read vacation
+            //var readVacation = _driver.FindElement(By.XPath("//div[@id='selectVacation']")).GetAttribute("value");
+            //Assert.AreEqual(_updatedVacation, readVacation);
+
+            // Read startdate, enddate and reason
+            var readStartDate = _driver.FindElement(By.XPath("//body//div[@role='dialog']//div//div//div//div[2]//div[1]//div[1]//input[1]")).GetAttribute("value");
+            var readEndDate = _driver.FindElement(By.XPath("//body/div[@role='dialog']/div/div/div/div/div[3]/div[1]/div[1]//input[1]")).GetAttribute("value");
+            var readReason = _driver.FindElement(By.XPath("//textarea[@id='txtInputReason']")).GetAttribute("value");
+            Assert.AreEqual(_updatedStartDate, readStartDate);
+            Assert.AreEqual(_updatedEnddate, readEndDate);
+            Assert.AreEqual(_updatedReason, readReason);
+        }
+
+        [TestMethod]
+        public void VacationCRUD_ST05_DeleteVacation()
+        {
+            _driver.Navigate().GoToUrl(_URL);
+
+            // Addressing first record
+            var sortByNew = _driver.FindElement(By.XPath("//div[contains(text(),'Id')]"));
+            sortByNew.Click(); sortByNew.Click();
+
+            // Delete first record
+            var deleteVacation = _driver.FindElement(By.XPath("//div[@role='rowgroup']//div[1]//div[6]//button[2]//*[name()='svg']//*[name()='path' and contains(@fill,'currentCol')]"));
+            deleteVacation.Click();
+
+            // Deleted vacation
+            var submitForm = _driver.FindElement(By.Id("submitVacationForm"));
+            submitForm.Click();
+
+            // Driver quit
+            _driver.Quit();
+        }
     }
 }
