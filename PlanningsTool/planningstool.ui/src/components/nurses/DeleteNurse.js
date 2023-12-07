204b7791b4452e7b36b2e83d6f8f7304c25d7ace
NabilEMAP <Nabil_EM@outlook.com>
2023-12-07 10:51:31 +0100

    Changed DeleteNurse Modal to read the selenium tests

--- PlanningsTool/planningstool.ui/src/components/nurses/DeleteNurse.js
+++ PlanningsTool/planningstool.ui/src/components/nurses/DeleteNurse.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Modal from 'react-bootstrap/Modal';
 import { toast } from 'react-toastify';
-import { Button, Checkbox, FormControl, FormControlLabel, IconButton, InputLabel, MenuItem, Select, Stack, TextField } from "@mui/material";
+import { Button, FormControl, IconButton, Stack } from "@mui/material";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { API_BASE_URL } from "../../config";
@@ -19,8 +19,6 @@ function DeleteNurse(props) {
   const [isFixedNight, setIsFixedNight] = useState(false);
   const [data, setData] = useState([]);
   const [regimeTypeData, setRegimeTypeData] = useState([]);
-  const [regime, setRegime] = useState([]);
-  const [fixedNight, setFixedNight] = useState([]);
 
   useEffect(() => {
     getData();
@@ -56,13 +54,10 @@ function DeleteNurse(props) {
       .then((result) => {
         setFirstName(result.data.firstName);
         setLastName(result.data.lastName);
-        setRegimeTypeId(result.data.regimeTypeId);
-        setIsFixedNight(result.data.isVasteNacht);
-        editId(id);
-
         const regime = regimeTypeData.find((item) => item.id === result.data.regimeTypeId);
-        setRegime(regime ? regime.name : '');
-        setFixedNight(result.data.isFixedNight ? 'Ja' : 'Nee');
+        setRegimeTypeId(regime ? regime.name : '');
+        setIsFixedNight(result.data.isFixedNight ? 'Ja' : 'Nee');
+        editId(id);
       })
       .catch((error) => {
         console.log(error);
@@ -109,9 +104,9 @@ function DeleteNurse(props) {
               <h4>Achternaam</h4>
               <p id="lastName" value={lastName}>{lastName}</p>
               <h4>Regime</h4>
-              <p id="regime" value={regime}>{regime}</p>
+              <p id="regime" value={regimeTypeId}>{regimeTypeId}</p>
               <h4>Vaste Nacht?</h4>
-              <p id="fixedNight" value={fixedNight}>{fixedNight}</p>
+              <p id="fixedNight" value={isFixedNight}>{isFixedNight}</p>
             </FormControl>
           </Stack>
         </Modal.Body>
